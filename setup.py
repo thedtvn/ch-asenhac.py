c00fc4ad8f3b9c912613ab7f4e5a2525bdfd81a5
duongtuan30306 <duongtuan30306@gmail.com>
2022-09-24 13:45:07 +0700

    update

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
    name='chíasenhac.py',
-   version="0.3.1",
-   description='A light weight Python library for the ZingMp3 API',
+   version="0.0.1",
+   description='A light weight Python library for the Chia Sẻ Nhạc',
    author='The DT',
    author_email='duongtuan30306@gmail.com',
    packages=["chíasenhac"],
