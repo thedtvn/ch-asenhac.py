71f43c379b775265c13769a33454329e8d0731c8
duongtuan30306 <duongtuan30306@gmail.com>
2022-09-24 13:46:18 +0700

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='chíasenhac.py',
+   name='chiasenhac.py',
    version="0.0.1",
    description='A light weight Python library for the Chia Sẻ Nhạc',
    author='The DT',
    author_email='duongtuan30306@gmail.com',
-   packages=["chíasenhac"],
+   packages=["chiasenhac"],
    install_requires=["aiohttp", "requests"]
 )
